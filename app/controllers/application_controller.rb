f405278289065439b28517ba161c9963c4186863
pusann <rainbow.7.7west2014@gmail.com>
2022-01-14 14:40:01 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_suth
+
+  private
+  def basic_auth
+    authenticate_or_request_with_thhp_basic do  |username, password|
+    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  end
 end
