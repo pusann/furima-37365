f8c4e7985395007c0ddabef2bc908a7855d68229
pusann <rainbow.7.7west2014@gmail.com>
2022-01-21 21:47:24 +0900

    最新2

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,6 +3,7 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
     console.log(priceInput);
   console.log("イベント発火");
+  priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     console.log(inputValue);
   const addTaxDom = document.getElementById("add-tax-price");
@@ -11,4 +12,5 @@ window.addEventListener('load', () => {
   const profitNumber = document.getElementById("profit")
      profitNumber.innerHTML = (Math.floor(inputValue - inputValue * 0.1));
      console.log(profitNumber);
+    })
 });
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,20 @@
 class Item < ApplicationRecord
    validates :product_name, presence: true
    validates :product_description, presence: true
-   validates :category_id , presence: true
-   validates :product_condition_id, presence: true
-   validates :burden_of_shipping_charges_id, presence: true
-   validates :shipping_area_id, presence: true
-   validates :days_to_ship_id, presence: true
-   validates :selling_price, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+   validates :category_id , presence: true,  numericality: { other_than: 1 , message: "can't be blank"}
+   validates :product_condition_id, presence: true,  numericality: { other_than: 1 , message: "can't be blank"}
+   validates :burden_of_shipping_charges_id, presence: true,  numericality: { other_than: 1 , message: "can't be blank"}
+   validates :shipping_area_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}  
+   validates :days_to_ship_id, presence: true,  numericality: { other_than: 1 , message: "can't be blank"}   
+   validates :selling_price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
+   validates :image,presence: true
   
    belongs_to :user
    has_many :comments
    has_one :purchase
    has_one_attached :image
 
-include ActiveHash::Associations
-  has_many :Category
+   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 end
  
