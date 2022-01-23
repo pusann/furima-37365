8e6688c46bc06cc9882e07c810ead822862c1fae
pusann <rainbow.7.7west2014@gmail.com>
2022-01-23 17:03:10 +0900

    修正3

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     burden_of_shipping_charges_id  { 2 }
     shipping_area_id               { 2 }
     days_to_ship_id                { 2 }
-    selling_price                  {'2000'}
+    selling_price                  { 2000 }
     association :user
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
