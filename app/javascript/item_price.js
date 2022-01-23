108a2775c29db364dfaaeccfdd409f14c70167dd
pusann <rainbow.7.7west2014@gmail.com>
2022-01-23 16:42:18 +0900

    修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,16 +1,10 @@
 window.addEventListener('load', () => {
-  //console.log("OK");
   const priceInput = document.getElementById("item-price");
-    //console.log(priceInput);
-  //console.log("イベント発火");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    //console.log(inputValue);
   const addTaxDom = document.getElementById("add-tax-price");
      addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-     //console.log(addTaxDom);
   const profitNumber = document.getElementById("profit")
      profitNumber.innerHTML = (Math.floor(inputValue - inputValue * 0.1));
-     //console.log(profitNumber);
     })
 });
