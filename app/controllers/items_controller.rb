a0c397725b32165558c035ba2acdceade6a13724
pusann <rainbow.7.7west2014@gmail.com>
2022-01-24 21:58:22 +0900

    最新

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new,]
+  #before_action :move_to_index, only: [:show]
+
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -25,7 +27,6 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:product_name,:product_description,:category_id,:product_condition_id,:burden_of_shipping_charges_id,:shipping_area_id,:days_to_ship_id,:selling_price,:image).merge(user_id: current_user.id)  
   end
+  
 end
-
-
 
