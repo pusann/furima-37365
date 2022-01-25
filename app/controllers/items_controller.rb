a57e34b83f63874a3e75ac83ca538bf1351553a5
pusann <rainbow.7.7west2014@gmail.com>
2022-01-25 17:24:05 +0900

    最新

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,]
+  before_action :authenticate_user!, only: [:new,:edit]
   #before_action :move_to_index, only: [:show]
 
   def index
@@ -18,7 +18,7 @@ class ItemsController < ApplicationController
   end
   def  edit  
     @item = Item.find(params[:id])
-      unless  current_user.id == @item.user.id
+      unless current_user.id ==  @item.user_id
         redirect_to action: :index
     end
   end
